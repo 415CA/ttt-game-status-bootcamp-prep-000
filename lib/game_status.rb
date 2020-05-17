0960ca99815b6011dcf20d38a999dfc40ef78886
Chris Allen <chrisallen415@gmail.com>
2020-05-17 19:14:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -23,20 +23,24 @@ def position_taken?(board, index)
 end
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
+  WIN_COMBINATIONS.detect do |win_combination|
+    win_index_0 = win_combination[0]
+    win_index_1 = win_combination[1]
+    win_index_2 = win_combination[2]
+    win_index_3 = win_combination[3]
+    win_index_4 = win_combination[4]
+    win_index_5 = win_combination[5]
+    win_index_6 = win_combination[6]
+    win_index_7 = win_combination[7]
 
+    position_0 = board[win_index_0]
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-
-    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 =="O" && position_2 == "O" && position_3 == "O"
-      return win_combination
-    end
+    position_4 = board[win_index_4]
+    position_5 = board[win_index_5]
+    position_6 = board[win_index_6] 
+    position_7 = board[win_index_7]
 
   end
-else
-  false
 end
