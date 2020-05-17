cc7cd5ebd04dc6586f89427314fa9eb37cf3dfcb
Chris Allen <chrisallen415@gmail.com>
2020-05-17 19:37:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -44,11 +44,13 @@ def full?(board)
 end
 
 def draw?(board)
-  full?(board) && !won?(board) ? true : false
-  elsif !full?(board) && !won?(board)
+  if !won?(board) && full?(board)
+    return true
+  elsif!full?(board) && !won?(board)
     return false
   else won?(board)
     return false
+  end
 end
 
 def over?(board)
